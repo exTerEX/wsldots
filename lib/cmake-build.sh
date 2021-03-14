f62f731ccb45e99e5fb2412d0a7d0f4c50ba6e9d
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-03-14 09:23:03 +0100

    Added cmake as submodule and a build script

---
+++ lib/cmake-build.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+apt update
+
+# Install build libraries
+apt --assume-yes install --no-install-recommends \
+    libcurl4-openssl-dev \
+    libexpat1-dev \
+    libjsoncpp-dev \
+    zlib1g-dev \
+    libbz2-dev \
+    liblzma-dev \
+    libnghttp2-dev \
+    libzstd-dev \
+    librhash-dev \
+    libuv1-dev \
+    libarchive-dev
+
+cp -r $(cd -P -- "$(dirname -- "$0")" && pwd -P)/cmake /tmp/cmake > /dev/null
+
+cd /tmp/cmake
+
+./bootstrap --system-libs --no-qt-gui --prefix=/usr/local
+make
+make install
+rm -r /tmp/cmake
+
+apt --assume-yes autoremove \
+    libcurl4-openssl-dev \
+    libexpat1-dev \
+    libjsoncpp-dev \
+    zlib1g-dev \
+    libbz2-dev \
+    liblzma-dev \
+    libnghttp2-dev \
+    libzstd-dev \
+    librhash-dev \
+    libuv1-dev \
+    libarchive-dev
+
+# Runtime
+apt install --no-install-recommends \
+    libarchive13 \
+    libcurl4 \
+    libexpat1 \
+    libjsoncpp1 \
+    librhash0 \
+    libuv1 \
+    procps \
+    zlib1g
