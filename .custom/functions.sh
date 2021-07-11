c148378fea920edd4a1e096d933c4a92f2f30f6a
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-07-11 13:32:20 +0200

    Simplified script and fixed functions

--- .custom/functions.sh
+++ .custom/functions.sh
@@ -1,11 +1,25 @@
 mkd () {
-    mkdir -p -- "$*" ; cd -- "$*" || exit ;
+    mkdir -p -- $* ; cd -- $* || exit ;
 }
 
 update () {
-    sudo apt update && sudo apt upgrade -y
+    if command -v apt > /dev/null 2>&1; then
+        sudo apt update && sudo apt upgrade -y
+    fi
 }
 
 upgrade () {
-    sudo apt update && sudo apt full-upgrade -y && upgrade_oh_my_bash
+    if command -v apt > /dev/null 2>&1; then
+        sudo apt update && sudo apt full-upgrade -y 
+    fi
+
+    if command -v upgrade_oh_my_bash > /dev/null 2>&1; then
+        upgrade_oh_my_bash
+    fi
+}
+
+gitc () {
+    git clone $*
+
+    cd `echo $* | sed -n -e 's/^.*\/\([^.]*\)\(.git\)*/\1/p'`
 }
